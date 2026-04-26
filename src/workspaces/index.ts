d33d31a218f6af1c359af7aa53a713a3edc13274
abdul rahman <amanasmuei@gmail.com>
2026-04-26 23:11:50 +0800

    feat(workspaces): public index re-exports

---
+++ src/workspaces/index.ts
@@ -0,0 +1,14 @@
+// src/workspaces/index.ts
+export * from "./types.js";
+export {
+  identifyWorkspace,
+  recordWorkspace,
+  listWorkspaces,
+  archiveWorkspace,
+  unarchiveWorkspace,
+  setNotes,
+  forgetWorkspace,
+  type ListOptions,
+  type WorkspaceId,
+} from "./tracker.js";
+export { storePath, loadStore, saveStore } from "./store.js";
